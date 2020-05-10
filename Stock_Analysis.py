d75c67a3d037df0cc44e19fd65cf76497e5a48ad
Kaushik Chatterjee <31332999+kchatr@users.noreply.github.com>
2020-05-10 09:05:15 -0400

    Added 50 and 200 moving average chart

--- Stock_Analysis.py
+++ Stock_Analysis.py
@@ -15,6 +15,9 @@ import matplotlib.pyplot as plt
 from matplotlib import style
 import pandas_datareader.data as pdr
 
+from pandas.plotting import register_matplotlib_converters
+register_matplotlib_converters()
+
 __author__ = "Kaushik Chatterjee"
 __maintainer__ = "Kaushik Chatterjee"
 __license__ = "MIT"
@@ -29,20 +32,31 @@ ticker = input("Please enter the ticker of the desired stock: ").upper()
 month = int(input("How many months back should we fetch the historic data? "))
 
 cur_date = datetime.datetime.today() #gets the current date 
-orig_date = pd.to_datetime(cur_date, format="%Y-%m-%d") - pd.DateOffset(months = month) #gets the date a year back
+orig_date = pd.to_datetime(cur_date, format="%Y-%m-%d") - pd.DateOffset(months = month) #gets the date the specified months back
 
 start = datetime.datetime(orig_date.year, orig_date.month, orig_date.day)
 end = datetime.datetime(cur_date.year, cur_date.month, cur_date.day)
 
+orig_date_2 = pd.to_datetime(cur_date, format="%Y-%m-%d") - pd.DateOffset(days = 200)
+start_2 = datetime.datetime(orig_date_2.year, orig_date_2.month, orig_date_2.day)
+end_2 = end
+
 df = pdr.DataReader(ticker, "yahoo", start, end)
+df_2 = pdr.DataReader(ticker, "yahoo", start_2, end_2)
 del df["Adj Close"]
+del df_2["Adj Close"]
 
 benchmark = pdr.DataReader("SPY", "yahoo", start, end)
 sp_closing = benchmark["Close"]
-
 closing_prices = df["Close"]
+closing_prices_2 = df_2["Close"]
+
+mvavg_50days = closing_prices_2.rolling(50, min_periods = 1).mean()
+mvavg_200days = closing_prices_2.rolling(200, min_periods = 1).mean()
+
 
 mvavg_plot = closing_prices.rolling(31, min_periods = 1).mean()
+emvavg_plot = closing_prices.ewm(halflife = 0.5, min_periods = 1).mean()
 std_dev = np.std(closing_prices)
 avg_vol = df["Volume"].mean()
 
@@ -52,7 +66,6 @@ stoch_osc_closing = df["Close"].tolist()
 stoch_osc_recentclose = stoch_osc_closing[len(stoch_osc_closing) - 1]
 stochastic_oscillator = ((stoch_osc_recentclose - stoch_osc_low) / (stoch_osc_high - stoch_osc_low)) * 100
 
-
 print("\nHere is a table showing the statistics of", ticker, "for the past week:")
 print(df.tail(14))
 print("\nThe volatility of", ticker, "is ${:.3f}".format(std_dev))
@@ -60,12 +73,11 @@ print("The average volume of", ticker, "is {:.3f}".format(avg_vol), "and the rel
 print("The stochastic oscillator of", ticker, "is %{:.3f}".format(stochastic_oscillator))
 
 plt.figure(1)
-vol = df.tail(7)["Volume"].tolist()
-label = [x for x in range(1, len(vol) + 1)]
-plt.bar(label, vol)
-plt.title("Volume Over Last 7 Days")
-plt.xlabel("Day No.")
-plt.ylabel("Volume")
+plt.plot(closing_prices_2, label = "Closing Prices of {0}".format(ticker))
+plt.plot(mvavg_50days, "g--", label = "50 Day Moving Average")
+plt.plot(mvavg_200days, "r--", label = "200 Day Moving Average")
+plt.title("Closing Prices vs. 50 Day Moving Average & 200 Day Moving Average")
+plt.legend()
 
 plt.figure(2)
 plt.scatter(closing_prices, sp_closing)
@@ -76,7 +88,8 @@ plt.title("{0} vs the S&P 500 ETF".format(ticker))
 plt.figure(3)
 closing_prices.plot(label = ticker)
 mvavg_plot.plot(label = "Moving Average")
-plt.title("Moving Average vs. Stock Prices")
+emvavg_plot.plot(label = "Exponential Moving Average")
+plt.title("Moving Average Over the Last {0} Month(s) vs. Stock Prices".format(month))
 plt.legend()
 
 plt.figure(4)
@@ -86,3 +99,4 @@ plt.title("Expected Returns Over %s Month(s)"%(month))
 plt.legend()
 
 plt.show()
+exit()
